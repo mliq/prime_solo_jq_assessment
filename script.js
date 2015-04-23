ed648739008efb7b61cbe94d98db2ddb29f222b4
mliq <liquori@gmail.com>
2015-04-23 15:38:45 -0500

    Step 2 complete.

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 var i = 0;
 
-$('document').ready(function(){
+$(document).ready(function(){
 
-   $('#generate').on('click',function(){
-       i++;
-       $('body').append('<div>Line #'+i+"<div id='changeColor'>Change Color</div><div id='remove'>Remove</div>");
-   });
+    $('#generate').on('click',function(){
+        i++;
+        $('body').append("<div class='line'>Line #"+i+
+            "<div id='changeColor'>Change Color</div><div id='remove'>Remove</div></div>");
+
+        $('#changeColor').on('click',function(){
+            $(this).closest('.line').css('background-color','orangered');
+        });
+    });
+
+
 
 });
